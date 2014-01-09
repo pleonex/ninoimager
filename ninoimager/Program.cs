85d7add05aa5324b6618da359045413b3f8592ca
pleonex <benito356@gmail.com>
2014-01-09 01:15:34 +0100

    Improved search and import mode writing output pack files to different folder.

--- ninoimager/Program.cs
+++ ninoimager/Program.cs
@@ -43,8 +43,8 @@ namespace Ninoimager
 			Stopwatch watch = new Stopwatch();
 			watch.Start();
 
-			if (args.Length == 3 && args[0] == "-ibg")
-				SearchAndImportBg(args[1], args[2]);
+			if (args.Length == 4 && args[0] == "-ibg")
+				SearchAndImportBg(args[1], args[2], args[3]);
 			else if (args.Length == 2 && args[0] == "-efr")
 				SearchAndExport(args[1]);
 			else
@@ -54,8 +54,11 @@ namespace Ninoimager
 			Console.WriteLine("It tooks: {0}", watch.Elapsed);
 		}
 
-		private static void SearchAndImportBg(string baseDir, string xmlList)
+		private static void SearchAndImportBg(string baseDir, string outputDir, string xmlList)
 		{
+			const string RootName    = "/Ninokuni.nds";
+			const string BaseRomPath = "/Ninokuni.nds/ROM/data/";
+
 			XDocument xml   = new XDocument();
 			xml.Declaration = new XDeclaration("1.0", "utf-8", "yes");
 			XElement root   = new XElement("NinoImport");
@@ -71,36 +74,45 @@ namespace Ninoimager
 				if (!match.Success)
 					continue;
 
-				string packFile = match.Groups[1] + ".n2d";
-				string relative = packFile.Replace(baseDir, "");
-				string infoFile = Path.Combine(Path.GetDirectoryName(file), ".info.xml");
-				if (!File.Exists(infoFile))
+				// Get paths
+				string imagePath = match.Groups[1].Value;
+				string relative  = imagePath.Replace(baseDir, "");
+				if (relative[0] == Path.DirectorySeparatorChar)
+					relative = relative.Substring(1);
+				string packFile  = Path.Combine(outputDir, relative) + ".n2d";
+				string outFile   = Path.Combine(outputDir, relative) + "_new.n2d";
+				string infoFile  = Path.Combine(Path.GetDirectoryName(packFile), ".info.xml");
+				if (!File.Exists(infoFile)) {
+					Console.WriteLine("## Warning ## '.info.xml' file not found.");
 					continue;
+				}
 
+				// Try to import
 				try {
 					Npck npck = Npck.ImportBackgroundImage(file);
-					npck.Write(packFile);
+					npck.Write(outFile);
 				} catch (Exception ex) {
-					Console.WriteLine("Error trying to import: {0}", relative);
+					Console.WriteLine("## Error ## Importing:  {0}", relative);
 					Console.WriteLine("\t" + ex.Message);
 					continue;
 				}
 
+				// Read info file to get ROM id
 				XDocument xinfo = XDocument.Load(infoFile);
 				string id = xinfo.Element("RomInfo").Descendants("File")
 							.First(f => f.Value == Path.GetFileName(packFile)).Attribute("Id").Value;
 
 				// Add game info
 				XElement xgame = new XElement("File");
-				xgame.Add(new XElement("Path", relative));
-				xgame.Add(new XElement("Import", relative));
+				xgame.Add(new XElement("Path", Path.Combine(BaseRomPath, relative) + ".n2d"));
+				xgame.Add(new XElement("Import", "{$ImagePath}/" + relative + "_new.n2d"));
 				gameInfo.Add(xgame);
 
 				// Add edit info
 				XElement xedit = new XElement("FileInfo");
-				xedit.Add(new XElement("Path", relative));
+				xedit.Add(new XElement("Path", Path.Combine(BaseRomPath, relative) + ".n2d"));
 				xedit.Add(new XElement("Type", "Common.Replace"));
-				xedit.Add(new XElement("DependsOn", "."));	// No idea?
+				xedit.Add(new XElement("DependsOn", RootName));
 				editInfo.Add(xedit);
 
 				Console.WriteLine("Written with success... {0}", relative);
