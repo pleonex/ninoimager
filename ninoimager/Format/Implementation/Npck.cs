a99b6e9f58cbbbf37759fa8a075d421375775363
pleonex <benito356@gmail.com>
2014-07-06 01:44:48 +0200

    Set transparent color from original palette

--- ninoimager/Format/Implementation/Npck.cs
+++ ninoimager/Format/Implementation/Npck.cs
@@ -374,7 +374,8 @@ namespace Ninoimager.Format
 				importer.DispCnt = ncgr.RegDispcnt;
 				importer.Quantization = new ManyFixedPaletteQuantization(nclr.GetPalettes());
 				importer.OriginalPalettes = nclr.GetPalettes();
-                importer.Format = nclr.Format;
+				importer.TransparentColor = nclr.GetPalette(0)[0];
+				importer.Format = nclr.Format;
                 if (nclr.Format == ColorFormat.Indexed_8bpp)
                     importer.PaletteMode = PaletteMode.Palette256_1;
                 else
